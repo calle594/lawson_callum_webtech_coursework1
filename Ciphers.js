d339d44414598f6169430d6903f8c1820e0471ef
Callum Lawson <46711152+calle594@users.noreply.github.com>
2019-03-01 22:37:56 +0000

    Add files via upload

--- Ciphers.js
+++ Ciphers.js
@@ -1,3 +1,4 @@
+
 function encryption() {    
 switch(document.getElementById("methods").value) {
     case "caesar":
@@ -11,7 +12,7 @@ switch(document.getElementById("methods").value) {
 
             if ((code >= 65) && (code <= 90))
                 c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-            else if ( (code >= 97) && (code <= 122) )
+            else if ((code >= 97) && (code <= 122) )
                 c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
 
             output += c;
@@ -21,21 +22,29 @@ switch(document.getElementById("methods").value) {
     }
 }
 
+
 function decryption(){
 switch(document.getElementById("methods").value) {
     case "caesar":
         var str = document.getElementById("text_encoded").value;
         var output = "";
-        var amount = str
-
+        var f = document.getElementById("text_decoded").value;
+        var key1 = Number(f)
+        
+        //loop through each character entered
         for (var i = 0; i < str.length; i ++) {
             var c = str[i];
+            //get character code of each letter
             var code = str.charCodeAt(i);
-
+            var shift = (document.getElementById("text_encoded").value);
+            
+            //uppercase letters
             if ((code >= 65) && (code <= 90))
-                c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-            else if ( (code >= 97) && (code <= 122) )
-                c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+                c = String.fromCharCode(((code - 65 + key1) % 26) + 26);
+            
+            //lowercase letters
+            else if ( (code >= 97) && (code <= 122))
+                c = String.fromCharCode(((code - 97 + key1) % 26) + 97);
 
             output += c;
         }
