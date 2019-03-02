708e1fd7deea22226ab584c567b9bea177ee6396
Callum Lawson <46711152+calle594@users.noreply.github.com>
2019-03-02 15:06:27 +0000

    Add files via upload

--- Ciphers.js
+++ Ciphers.js
@@ -3,17 +3,17 @@ function encryption() {
 switch(document.getElementById("methods").value) {
     case "caesar":
         var str = document.getElementById("plain_text").value;
-        var amount = Number(prompt("Number of shift"));
         var output = "";
-
+        var shift = parseInt(document.getElementById("key").value);
+        
         for (var i = 0; i < str.length; i ++) {
             var c = str[i];
             var code = str.charCodeAt(i);
 
             if ((code >= 65) && (code <= 90))
-                c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+                c = String.fromCharCode((code - 65 + shift) % 26 + 65);
             else if ((code >= 97) && (code <= 122) )
-                c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+                c = String.fromCharCode((code - 97 + shift) % 26 + 97);
 
             output += c;
         }
@@ -28,23 +28,21 @@ switch(document.getElementById("methods").value) {
     case "caesar":
         var str = document.getElementById("text_encoded").value;
         var output = "";
-        var f = document.getElementById("text_decoded").value;
-        var key1 = Number(f)
+        var shift = parseInt(document.getElementById("key").value);
         
         //loop through each character entered
         for (var i = 0; i < str.length; i ++) {
             var c = str[i];
             //get character code of each letter
             var code = str.charCodeAt(i);
-            var shift = (document.getElementById("text_encoded").value);
             
             //uppercase letters
             if ((code >= 65) && (code <= 90))
-                c = String.fromCharCode(((code - 65 + key1) % 26) + 26);
+                c = String.fromCharCode((code - 65 + 26 - shift) % 26 + 65);
             
             //lowercase letters
             else if ( (code >= 97) && (code <= 122))
-                c = String.fromCharCode(((code - 97 + key1) % 26) + 97);
+                c = String.fromCharCode((code - 97 + 26 - shift) % 26 + 97);
 
             output += c;
         }
