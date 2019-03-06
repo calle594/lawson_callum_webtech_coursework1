cf964369a1ef9516e4111a033e961d36d0629198
Callum Lawson <46711152+calle594@users.noreply.github.com>
2019-03-06 18:25:54 +0000

    Add files via upload

---
+++ Coursework 6-3-19/Ciphers.js
@@ -0,0 +1,149 @@
+function encryption() {    
+switch(document.getElementById("methods").value) {
+    case "caesar":
+        var str = document.getElementById("plain_text").value;
+        var output = "";
+        var shift = parseInt(document.getElementById("key").value);
+        
+         //loop through each character entered
+        for (var i = 0; i < str.length; i ++) {
+            var c = str[i];
+            //get character code of each letter
+            var code = str.charCodeAt(i);
+            
+            //uppercase letters
+            if ((code >= 65) && (code <= 90))
+                c = String.fromCharCode((code - 65 + shift) % 26 + 65);
+               
+            //lowercase letters
+            else if ((code >= 97) && (code <= 122) )
+                c = String.fromCharCode((code - 97 + shift) % 26 + 97);
+
+            output += c;
+        }
+        document.getElementById("text_encoded").value=output;
+        break;
+    }
+}
+
+
+function decryption(){
+switch(document.getElementById("methods").value) {
+    case "caesar":
+        var str = document.getElementById("text_encoded").value;
+        var output = "";
+        var shift = parseInt(document.getElementById("key").value);
+        
+        //loop through each character entered
+        for (var i = 0; i < str.length; i ++) {
+            var c = str[i];
+            //get character code of each letter
+            var code = str.charCodeAt(i);
+            
+            //uppercase letters
+            if ((code >= 65) && (code <= 90))
+                c = String.fromCharCode((code - 65 + 26 - shift) % 26 + 65);
+            
+            //lowercase letters
+            else if ( (code >= 97) && (code <= 122))
+                c = String.fromCharCode((code - 97 + 26 - shift) % 26 + 97);
+
+            output += c;
+        }
+        document.getElementById("text_decoded").value=output;
+        break;
+    }
+}
+
+function base_64(){
+switch(document.getElementById("methods").value){
+    case "base-64":    
+        var input = document.getElementById("plain_text");
+
+        output = window.btoa(input.value); //Create base-64 ASCII string
+        
+        document.getElementById("text_encoded").value = output;
+        break;
+    }
+}
+
+function dec_base_64(){
+switch(document.getElementById("methods").value){
+    case "base-64":    
+        var input = document.getElementById("text_encoded");
+
+        output = window.atob(input.value);// Decode string which has been encoded with base-64
+        
+        document.getElementById("text_decoded").value = output;
+        break;
+    }
+}
+
+function morse(){
+switch(document.getElementById("methods").value){
+    case "morse":
+        var letters =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,?:-!&'@()=:/+\"0123456789$_;";
+        var morseletters = new Array (
+    ".-","-...","-.-.","-..",".","..-.",
+    "--.","....","..",".---","-.-",".-..",
+    "--","-.","---",".--.","--.-",".-.",
+    "...","-","..-","...-",".--","-..-",
+    "-.--","--..",".-","-...","-.-.","-..",
+    ".","..-.","--.","....","..",".---",
+    "-.-",".-..","--","-.","---",".--.",
+    "--.-",".-.","...","-","..-","...-",
+    ".--","-..-","-.--","--.."," ",".-.-.-",
+    "--..--","..--..","---...","-....-","-.-.--",".-...",".----.",".--.-.","-.--.-","-.--.","-...-","---...","-..-.",".-.-.",".-..-.","-----",".----","..---","...--","....-",".....","-....","--...","---..","----.","...-..-","..--.-","-.-.-");
+        
+        var x = document.getElementById("plain_text").value;
+        var output = "";
+        
+        for(count = 0; count < x.length; count++) {
+        Char = x.charAt(count);
+        for (i = 0; i < letters.length; i++) {
+        if (Char == letters.charAt(i)) {
+        output += morseletters[i] ;    
+            }
+        }
+    }
+    document.getElementById("text_encoded").value = output;
+    break;
+    }
+}
+
+function dec_morse(){
+switch(document.getElementById("methods").value){
+    case "morse":
+        var letters =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,?:-!&'@()=:/+\"0123456789$_;";
+        var morseletters = new Array (
+    ".-","-...","-.-.","-..",".","..-.",
+    "--.","....","..",".---","-.-",".-..",
+    "--","-.","---",".--.","--.-",".-.",
+    "...","-","..-","...-",".--","-..-",
+    "-.--","--..",".-","-...","-.-.","-..",
+    ".","..-.","--.","....","..",".---",
+    "-.-",".-..","--","-.","---",".--.",
+    "--.-",".-.","...","-","..-","...-",
+    ".--","-..-","-.--","--.."," ",".-.-.-",
+    "--..--","..--..","---...","-....-","-.-.--",".-...",".----.",".--.-.","-.--.-","-.--.","-...-","---...","-..-.",".-.-.",".-..-.","-----",".----","..---","...--","....-",".....","-....","--...","---..","----.","...-..-","..--.-","-.-.-");
+        
+        var x = document.getElementById("text_encoded").value;
+        var output = "";
+        
+        for(count = 0; count < x.length; count++) {
+        Char = x.charAt(count);
+        for (i = 0; i < letters.length; i++) {
+        if (Char == letters.charAt(i)) {
+        output += morseletters[i] ;    
+            }
+        }
+    }
+    document.getElementById("text_decoded").value = output;
+    break;
+    }
+}
+
+
+ 
+        
+  
